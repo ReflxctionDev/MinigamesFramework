346606313af797dd96729c0c74542bb11a971851
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2020-12-13 11:18:00 +0200

    Add files

--- copier.bat
+++ copier.bat
@@ -1,3 +1,3 @@
 @echo off
-copy "D:\Java\SillyGames\build\libs\SillyGames-1.0-SNAPSHOT.jar" "D:\Minecraft\Servers\1.12.2\plugins\SillyGames-1.0-SNAPSHOT.jar" /Y
+copy "D:\Java\SillyGames\build\libs\MinigamesFramework-1.0-SNAPSHOT.jar" "D:\Minecraft\Servers\1.12.2\plugins\MinigamesFramework-1.0-SNAPSHOT.jar" /Y
 echo Copied to versions
